7dba2dbef883e43b5dc8d1c422577f487c267fbd
alexrodriguez <alexcarvajal2404@gmail.com>
2024-06-21 13:14:59 -0500

    uso dinamico de api

--- src/app/api/auth/user/getUserInSession/route.ts
+++ src/app/api/auth/user/getUserInSession/route.ts
@@ -2,6 +2,8 @@ import {NextRequest, NextResponse} from "next/server";
 import {decodeJwt} from "@clerk/backend/jwt";
 import {clerkClient} from "@clerk/nextjs/server";
 
+export const dynamic = 'force-dynamic'
+
 export async function GET(req: NextRequest) {
     try {
         const cookies = req.cookies;
