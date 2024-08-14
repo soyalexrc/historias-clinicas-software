3be1db0b2a1dbcae58044bfe9f1d95eb40e42244
alexrodriguez <alexcarvajal2404@gmail.com>
2024-08-14 13:01:02 -0500

    method for ticketinfoDetail table insert remote

--- src/app/api/ticket/remoteInsert/infoDetail/route.ts
+++ src/app/api/ticket/remoteInsert/infoDetail/route.ts
@@ -1,19 +1,40 @@
 import {NextRequest, NextResponse} from "next/server";
 import prisma from "@/lib/db/prisma";
 
+type IncomingData = {
+    C_ID: number,
+    C_ID_ITEM: string,
+    C_DESCRIP_ITEM: string,
+    C_VALOR_VENTA_UNITARIA: number,
+    C_CANTIDAD_ITEM: number,
+    C_IGV_TOTAL_ITEM: number,
+    C_COD_SUNAT_PROD_SERV_ITEM: string,
+}
 
 export async function POST(req: NextRequest) {
     try {
-        const data = await req.json();
-        const newTicket = await prisma.testTicket.create({
-            data: {
-                jsonString: JSON.stringify(data)
-            }
+        const body: IncomingData[] = await req.json();
+        const newTicket = await prisma.ticketInfoDetail.create({
+            data: body[0]
         })
 
-        return NextResponse.json({ message: 'Se registro el ticket remoto con exito!' })
+        return NextResponse.json({message: 'Se registro el ticket remoto con exito!'})
     } catch (error) {
         console.error(error);
-        return NextResponse.json({ error: JSON.stringify(error) })
+        return NextResponse.json({error: JSON.stringify(error)})
     }
 }
+
+export async function GET(req: NextRequest) {
+    try {
+        // TODO agregar filtros por query params/body...
+        const data = await prisma.ticketInfoDetail.findMany();
+
+        return NextResponse.json(data)
+    } catch (error) {
+        console.error(error);
+        return NextResponse.json({error: JSON.stringify(error)})
+    }
+}
+
+
